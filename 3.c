0db2b5d7b703865510a306055e1e81bc55f0efb1
Sean McBride <spmcbride1201@gmail.com>
2019-02-13 22:30:55 -0500

    feat: implement first part of problem 3

--- 3.c
+++ 3.c
@@ -1,2 +1,42 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// int _findMissNo(int count, int nums[], int commonDifference)
+// {
+//     if
+//         count
+// }
+
 /* Find the missing number from a given array. Your array will be in form [n, n+c, n+2c, …]. */
-int findMissNo(int nums[])
+int findMissNo(int count, int nums[])
+{
+    // find the difference of the arithmetic series.
+    int commonDifference = 0;
+
+    for (int i = 0; i <= count - 1; i++)
+    {
+        if (commonDifference != 0 && nums[i] + commonDifference == nums[i + 1])
+        {
+            break;
+        }
+        else
+        {
+            commonDifference = nums[i + 1] - nums[i];
+        }
+    }
+
+    for (int i = 1; i < count; i++)
+    {
+        if (nums[i] > nums[0] + i * commonDifference)
+        {
+            return nums[0] + i * commonDifference;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int test[] = {1, 3, 5, 9};
+    printf("Missing %d\n", findMissNo(sizeof(test) / sizeof(test[0]), test));
+}
